7952d28380ec3e982053cb3a1a2fe0a6642a7be3
tomiwaaladebo <tomiwa.aladebo@yahoo.com>
2022-10-14 23:53:59 -0700

    puts correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
- * main - print using puts
- * return: 0
- */
+/**
+ * main -print using puts
+ * Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
